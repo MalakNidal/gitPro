e37d7589ba76113f96107259b4ccb31c76a7d455
Malak Awawdeh <malak.awawdeh@stu.najah.edu>
2020-11-25 12:35:58 +0200

    we create a new repository

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gitPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("koli 5ra ya 7la");
+        }
+    }
+}
