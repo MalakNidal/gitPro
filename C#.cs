4ab4e2a3e2ffe2e5c814bfe73de824836ea8349e
Malak Awawdeh <malak.awawdeh@stu.najah.edu>
2020-11-25 13:07:19 +0200

    New

---
+++ C#.cs
@@ -0,0 +1,10 @@
+public class malak
+{
+    static void main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+         {
+             Console.WriteLine("I =" +i);
+         }
+    }
+}
---
+++ Pgogram.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace gitPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
